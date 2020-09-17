93daade01b607c205c3f5ee26fe32cda8b6c16e2
martinRenou <martin.renou@gmail.com>
2020-09-17 11:21:56 +0200

    Remove docs bundle
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -63,28 +63,6 @@ module.exports = [
     },
     externals,
     resolve,
-  },
-
-
-  /**
-   * Documentation widget bundle
-   *
-   * This bundle is used to embed widgets in the package documentation.
-   */
-  {
-    entry: './src/index.ts',
-    output: {
-      filename: 'embed-bundle.js',
-      path: path.resolve(__dirname, 'docs', 'source', '_static'),
-      library: "ipygany",
-      libraryTarget: 'amd'
-    },
-    module: {
-      rules: rules
-    },
-    devtool: 'source-map',
-    externals,
-    resolve,
   }
 
 ];
